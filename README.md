2166a42a2f8a0e48058c5b08afc000055f5d3838
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2018-04-24 16:43:13 -0700

    fixed all errors, project complete

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# week-4-game-crystal-new
+# Crystals Collector Game
+
+Click Here to Play: [https://juliajyshin.github.io/Crystals-Collector-Game/](https://juliajyshin.github.io/Crystals-Collector-Game/)
+
+## Project Description
+
+This is a fun and interactive number guessing game for web browsers. In this app the HTML pages will dynamically update with the jQuery library. To play click the Crystals, which contain a hidden value (1-12), to try to match your total score with the number to guess. To restart your wins and losses score, refresh the page. 
+
+![An image of the game](assets/images/crystals-game.png)
+
+## Technologies
+
+* HTML
+* CSS
+* jQuery
+
+## Author
+* https://github.com/juliajyshin
+* [Buy me a coffee ☕️](https://ko-fi.com/juliajverie)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,86 @@
-$(document).ready(function() {
 
-var  wins = 0;
-var losses = 0;
-var myNumber = 0;
-var targetNumber = randomNumGen();
-
-var crystal = {
-        diamond: {
-            name: "Diamond",
-            value: Math.floor(Math.random() * 12) + 1,
-        },
-        ruby: {
-            name: "Ruby",
-            value: Math.floor(Math.random() * 12) + 1,
-        },
-        sapphire: {
-            name: "Sapphire",
-            value: Math.floor(Math.random() * 12) + 1,
-        },
-        zircon: {
-            name: "Zircon",
-            value: Math.floor(Math.random() * 12) + 1,
-        }
-    };
-
+// Exexucte this code when DOM is fully loaded
+$(document).ready(function () {
+  // Setting up our target variables
+  var wins = 0;
+  var losses = 0;
+  var myNumber = 0;
+  var targetNumber = randomNumGen();
+  // Generates random variables for our crystals
+  var crystal = {
+    diamond: {
+      name: "Diamond",
+      value: Math.floor(Math.random() * 12) + 1,
+    },
+    ruby: {
+      name: "Ruby",
+      value: Math.floor(Math.random() * 12) + 1,
+    },
+    sapphire: {
+      name: "Sapphire",
+      value: Math.floor(Math.random() * 12) + 1,
+    },
+    zircon: {
+      name: "Zircon",
+      value: Math.floor(Math.random() * 12) + 1,
+    }
+  };
+  // Function to create a random number between 19 and 120
   function randomNumGen() {
-      return Math.floor(Math.random() * 102) + 19;
-      $("guess-number").text(targetNumber);
-    };
+    return Math.floor(Math.random() * 102) + 19;
+  };
 
-  var setGame = function() {
-      myNumber = 0;
-      targetNumber = randomNumGen();
-      $("guess-number").text(targetNumber);
-    };
+  // Function that resets the game 
+  var setGame = function () {
+    // Make our current total number 0
+    myNumber = 0;
+    // Change tartget number everytime the game resets
+    targetNumber = randomNumGen();
+    // Display our target number on page
+    $("#guessing-number").text(targetNumber);
+  };
 
-  console.log("Score Number: " + myNumber);
-  console.log("TARGET NUMBER " + targetNumber);
+  // If the users number is larger than the target number (lose)
+  var checkWin = function () {
+    if (myNumber > targetNumber) {
+      // Show victory message, restart the game, and render new number to guess
+      alert("Sorry. You lost!");
+      // Add 1 point to losing score
+      losses++;
+      $("#count-losses").text(losses);
+      setGame();
+    }
+    // If the users number is equal to the target number (win)
+    else if (myNumber === targetNumber) {
+      // Show victory message, restart the game, and render new number to guess
+      alert("Congratulations! You Won!");
+      // Add 1 point to winning score
+      wins++;
+      $("#count-wins").text(wins);
+      setGame();
+    }
+  };
 
-var checkWin = function() {
-  if (myNumber > targetNumber) {
-    alert("Sorry. You lost!");
-    losses++;
-    $("#count-losses").text(losses);
-    setGame();
-  }
+  // Calls our functions to start the game
+  setGame();
 
-  else if (myNumber === targetNumber) {
-    alert("Congratulations! You Won!");
-    wins++;
-    $("#count-wins").text(wins);
-    setGame();
-  }
-};
+  // Grab random values add it to each crystal
+  var addValues = function (clickedCrystal) {
+    myNumber += clickedCrystal.value;
+    $("#score-number").text(myNumber);
+    checkWin();
+  };
 
-setGame();
-
-
-var addValues = function(clickedCrystal) {
-  myNumber += clickedCrystal.value;
-  $("#score-number").text(myNumber);
-  checkWin();
-};
-
-
-
-  $("#diamond").click(function() {
+  $("#diamond").click(function () {
     addValues(crystal.diamond);
   });
-  $("#ruby").click(function() {
+  $("#ruby").click(function () {
     addValues(crystal.ruby);
   });
-  $("#sapphire").click(function() {
+  $("#sapphire").click(function () {
     addValues(crystal.sapphire);
   });
-  $("#zircon").click(function() {
+  $("#zircon").click(function () {
     addValues(crystal.zircon);
   });
 
